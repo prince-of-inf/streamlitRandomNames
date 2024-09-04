72fc54a6700288e1aaa5b3a9b2b96478130a2612
prince-of-inf <bzwol@proton.me>
2024-09-04 11:24:01 +0200

    fixed history

--- 1_main.py
+++ 1_main.py
@@ -6,14 +6,21 @@ import os
 
 directory_path = "names/"
 nations = [str(file)[:-4] for file in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, file))]
-
+nations = sorted(nations)
 # Define the available nations
 
 
 # Sidebar layout
+st.sidebar.title("How to use:")
+st.sidebar.markdown(" • Select nations")
+st.sidebar.markdown(" • Adjust the order, chaos, and other options as needed")
+st.sidebar.markdown(" • Select the display style from the radio buttons")
+st.sidebar.markdown(" • Click the 'Generate Names' button to generate random names")
+
 st.sidebar.title("Options")
 selected_nations = st.sidebar.multiselect("Select Nations", nations)
-ngram_order = st.sidebar.slider("Randomness - lower means more random", 2, 5, 3)
+ngram_order = st.sidebar.slider("ORDER 🡢 higher order = closer to existing names", 1, 4, 3) + 1
+chaos_fact_flag = st.sidebar.slider("CHAOS 🡢 higher chaos = more random", 0.0, 1.0, 0.2, step=0.1)
 # num_names = st.sidebar.slider("Number of Names", 1, 100, 10)
 name_exist_flag = st.sidebar.checkbox("Only Non-existing Names", False)
 opt1 = "Default"
@@ -21,16 +28,18 @@ opt2 = "List"
 opt3 = "Table"
 display_style = st.sidebar.radio("Display style", [opt1, opt2, opt3])
 
-
 # Main content
 st.title("RPG Name Generator App")
+
+
+
 generate_button = st.button("Generate Names")
 
 # IF at least one nation is selected, generate names
 if selected_nations and generate_button:
     names = load_names(selected_nations)
     df, starts = prepare_ngrams(names, ngram_order)
-    generated_names = generate_names_markov_chain(df, starts, names, ngram_order, 36, name_exist_flag, return_list=True)
+    generated_names = generate_names_markov_chain(df, starts, names, ngram_order, 36, chaos_fact_flag, name_exist_flag)
 
     st.subheader("Generated Names:")
     # st.write(pd.DataFrame(generated_names))
@@ -64,5 +73,17 @@ if selected_nations and generate_button:
                      height=500,
                      width=500)
 
-else:
-    st.info("Please select at least one nation.")
+if len(selected_nations)<1:
+    st.info("Select at least one nation in the left sidebar.")
+# Shake the info text
+import time
+def shake_info_text(text):
+    for _ in range(5):
+        st.info("Please select at least one nation.")
+        st.markdown(f"<span style='color:red'>{text}</span>", unsafe_allow_html=True)
+        time.sleep(0.1)
+        st.markdown(f"<span style='color:black'>{text}</span>", unsafe_allow_html=True)
+        time.sleep(0.1)
+if len(selected_nations)<1 and generate_button:
+    text = "Please select at least one nation."
+    st.markdown(f"<span style='color:red'>{text}</span>", unsafe_allow_html=True)
